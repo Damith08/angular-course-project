aac9ad020b09dfd547a0307b4e1bbb44a8b86559
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-06-27 12:46:35 +0530

    feat: cleanup
    Section 12 - Project Cleanup

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -16,7 +16,6 @@ export class RecipeEditComponent {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.editMode = params['id'] != null;
-      console.log(this.editMode);
     });
   }
 }
