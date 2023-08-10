892a413a0a2dc203b0465f3883905d47646effcf
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-08-10 10:36:56 +0530

    feat: more lazy loading
    Section 22 - More Lazy Loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -18,10 +16,8 @@ import { AuthModule } from './auth/auth.module';
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
-    ShoppingListModule,
     SharedModule,
     CoreModule,
-    AuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
